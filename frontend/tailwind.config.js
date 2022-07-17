b27244e4397a7e726fc7edc04e142ba3db6a89a2
MohamedenVal <med4en@gmail.com>
2022-07-17 21:21:12 +0000

    Finished the navigation and skeleton of the app

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -3,8 +3,28 @@ module.exports = {
   content: [
     "./src/**/*.{html,ts}",
   ],
+  darkMode: 'class',
+  mode: 'jit',
   theme: {
-    extend: {},
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '1rem',
+        sm: '2rem',
+        md: '3rem',
+        lg: '4rem'
+      },
+    },
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: '#00adb5',
+          black: '#222831',
+          gray: '#393e46',
+          white: '#eeeeee'
+        },
+      }
+    },
   },
   plugins: [],
 }
